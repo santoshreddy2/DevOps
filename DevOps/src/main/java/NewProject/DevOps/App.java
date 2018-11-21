4aecd369e7435d70e98fb26741d53eed0866b1f2
santoshreddy2 <anreddysantosh@gmail.com>
2018-11-21 14:54:54 -0800

    santhu

---
+++ DevOps/src/main/java/NewProject/DevOps/App.java
@@ -0,0 +1,13 @@
+package NewProject.DevOps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
